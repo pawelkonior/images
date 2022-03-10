e388077f988d8de95396c26a0276b368a2b081e9
Paweł Konior <pawel.konior@gmail.com>
2022-03-10 13:46:13 +0100

    custom user and super user migrations

---
+++ backend/accounts/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+
+CustomUser = get_user_model()
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +127,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'accounts.CustomUser'
